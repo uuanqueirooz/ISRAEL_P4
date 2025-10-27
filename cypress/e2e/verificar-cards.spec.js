db22731cd8e990d5621a006b30abd95893e673d0
Wanderson <uuanqueiroz@gmail.com>
2025-10-27 18:41:08 -0300

    mais uma correcao

--- cypress/e2e/verificar-cards.spec.js
+++ cypress/e2e/verificar-cards.spec.js
@@ -5,66 +5,37 @@ describe('Verificação da Página de QA', () => {
   });
 
   it('Deve ter o título principal correto', () => {
-    // Verifica o h1, limpando os hífens que estão no HTML
-    cy.get('header h1')
-      .invoke('text')
-      .then(text => {
-        const normalizedText = text.replace(/-/g, '').trim();
-        // O h1 no HTML é "Jornada do QA"
-        expect(normalizedText).to.equal('Jornada do QA');
-      });
+    // Verifica se o h1 contém o texto esperado. É mais simples e direto.
+    cy.get('header h1').should('have.text', 'Jornada do QA');
   });
 
-  // Este teste verifica os 6 cards originais
-  it('Deve exibir todos os 6 cards com os títulos corretos', () => {
-    
+  it('Deve exibir todos os 7 cards com os títulos corretos', () => {
     const expectedTitles = [
       'Passo 1: Entenda os Fundamentos',
       'Passo 2: Aprenda Conceitos Chave',
       'Passo 3: Domine as Ferramentas',
-      'Passo 4: Desenvolva Soft Skills', // Corrigido para "Desenvolva" com "a"
+      'Passo 4: Desenvolva Soft Skills',
       'Passo 5: Lógica e SQL',
       'Passo 6: Pratique!',
-      'Bônus: Recursos Adicionais' // Título do novo card
+      'Bônus: Recursos Adicionais'
     ];
 
-    cy.get('.card h2').as('cardTitles');
-    
-    // Procura por 6 cards, já que o bônus está comentado no HTML
-    cy.get('@cardTitles').should('have.length', 7);
+    cy.get('.card h2').should('have.length', expectedTitles.length);
 
-    cy.get('@cardTitles').each((titleElement, index) => {
-      
-      const expectedText = expectedTitles[index];
-
-      cy.wrap(titleElement)
-        .invoke('text') 
-        .then((actualText) => {
-          // Limpa espaços em branco extras
-          const normalizedText = actualText.replace(/\s+/g, ' ').trim();
-          // Compara o texto limpo
-          expect(normalizedText).to.equal(expectedText);
-        });
+    cy.get('.card h2').each((cardTitle, index) => {
+      cy.wrap(cardTitle).should('have.text', expectedTitles[index]);
     });
   });
 
-
-  
-   //---- NOVO TESTE (COMENTADO) ----
-  // Este teste verifica especificamente os links do novo card.
-  // Para usá-lo, descomente este bloco e o card no 'exercicio.html'.
-  
   it('Deve verificar os links no card de Bônus', () => {
-    
     // 1. Encontra o card de bônus pelo seu h2
     cy.get('.card')
       .contains('h2', 'Bônus: Recursos Adicionais')
+      .parent() // Sobe para o elemento .card
       .as('bonusCard');
 
     // 2. Pega todos os links (tags 'a') dentro desse card
-    cy.get('@bonusCard')
-      .find('ul li a')
-      .as('links');
+    cy.get('@bonusCard').find('ul li a').as('links');
 
     // 3. Verifica se são 3 links
     cy.get('@links').should('have.length', 3);
@@ -72,17 +43,17 @@ describe('Verificação da Página de QA', () => {
     // 4. Verifica se os links têm os atributos corretos
     cy.get('@links').eq(0)
       .should('have.text', 'Ministry of Testing')
-      .and('have.attr', 'href', 'https_//www.ministryoftesting.com/')
+      .and('have.attr', 'href', 'https://www.ministryoftesting.com/')
       .and('have.attr', 'target', '_blank');
       
     cy.get('@links').eq(1)
       .should('have.text', 'Software Testing Help')
-      .and('have.attr', 'href', 'https_//www.softwaretestinghelp.com/')
+      .and('have.attr', 'href', 'https://www.softwaretestinghelp.com/')
       .and('have.attr', 'target', '_blank');
 
     cy.get('@links').eq(2)
       .should('have.text', 'Cursos de Teste de Software na Udemy')
-      .and('have.attr', 'href', 'https_//www.udemy.com/topic/software-testing/')
+      .and('have.attr', 'href', 'https://www.udemy.com/topic/software-testing/')
       .and('have.attr', 'target', '_blank');
   });
   
